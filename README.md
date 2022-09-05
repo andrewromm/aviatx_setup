fffa7163b55ab3a928a27d4a5bf5e78ca2bed064
Andrey Romm <andreyromm@Andreys-MacBook-Air.local>
2022-09-05 17:18:09 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Setup
+
+curl -s https://raw.githubusercontent.com/andrewromm/aviatx_setup/master/setup.sh | sudo bash 
--- setup.sh
+++ setup.sh
@@ -2,7 +2,15 @@
 
 VERSION=2.0.1
 BOOTSTRAP_BRANCH=${BRANCH:-master}
+BOOTSTRAP_DIR=/srv/aviatx/bootstrap
+FACT_CONF=/etc/ansible/facts.d/config.fact
 INSTALL_LOG=$(mktemp /tmp/aviatx-setup.XXXXXXXX)
+
+# state vars
+DOMAIN=""
+INSTALLED=""
+DEF_HOSTALIAS="aviatx"
+HOSTALIAS="$DEF_HOSTALIAS"
 
 ################################################################################
 # Library
@@ -69,3 +77,137 @@ if [ ! "$UID" -eq 0 ]; then print_error "Run as root or insert 'sudo -E' before 
 # check debian-like
 if [[ ! -f /etc/debian_version ]]; then print_error "Target OS is Ubutu 20.04"; exit 1; fi
 
+################################################################################
+## Dialogs
+################################################################################
+
+show_dialog(){
+  whiptail --title "$1" --msgbox "$2" 8 78
+}
+
+whiptailInput() {
+    eval local init="\$$1"
+    case "$1" in
+        *PASSWORD*) local prompt='passwordbox'; showval="" ;;
+        *) local prompt='inputbox'; showval=$init; ;;	
+    esac
+    local value=$(whiptail --title "$2" --${prompt} "$3" $4 $5 $showval 3>&1 1>&2 2>&3)
+    local rc=$?
+    if [ $rc = 0 ]; then
+      if [ $prompt == 'passwordbox' ]; then
+        local confirmation=$(whiptail --title "$2 / confirmation" --${prompt} "$3" $4 $5 $showval 3>&1 1>&2 2>&3)
+        local rc=$?
+        if [ $rc = 0 ]; then
+          if [ "$value" != "$init" ]; then
+            if [ $value == $confirmation ]; then
+              if [[ -n ${value// } ]]; then
+                enc=$(openssl passwd -apr1 ${value})
+                eval $1="'$enc'"
+                save_config
+              fi
+            fi
+          fi
+        fi
+      else
+        eval $1="'$value'"
+        save_config
+      fi
+    fi
+}
+
+request_domain(){
+  whiptailInput "DOMAIN" "Domain" "Please enter domain for your tracker." 8 78
+}
+
+request_hostalias(){
+  whiptailInput "HOSTALIAS" "Short hostname" "Shot server hostname that you can see at command line prompt." 8 78
+}
+
+################################################################################
+## Configs
+################################################################################
+
+load_config(){
+  if [ -a "$FACT_CONF" ]; then
+    INSTALLED=$(awk -F "=" '/installed/ {print $2}' $FACT_CONF)
+    DOMAIN=$(awk -F "=" '/domain/ {print $2}' $FACT_CONF)
+    HOSTALIAS=$(awk -F "=" '/hostalias/ {print $2}' $FACT_CONF)
+    if [[ -z "$HOSTALIAS" ]]; then HOSTALIAS=$DEF_HOSTALIAS; fi
+  fi
+}
+
+save_inventory(){
+  cd "$BOOTSTRAP_DIR"
+  echo """
+[private]
+${HOSTALIAS} ansible_host=${DOMAIN}
+
+[aviatx]
+${HOSTALIAS}
+""" > inventory/private
+}
+
+save_config(){
+  save_inventory \
+  && mkdir -p $(dirname $FACT_CONF) \
+  && echo """[general]
+domain=${DOMAIN}
+hostalias=${HOSTALIAS}
+installed=${INSTALLED}""" > $FACT_CONF
+}
+
+################################################################################
+## Executing
+################################################################################
+
+initialize(){
+  while [ -z "${DOMAIN// }" ]; do request_domain
+  done
+  while [ -z ${HOSTALIAS// } ]; do request_hostalias
+  done
+}
+
+if [[ -a "$FACT_CONF" ]]; then
+  print_status "Reading local configuration"
+  load_config
+fi
+
+print_status "Preparing system"
+if [ "$INSTALLED" == "$VERSION" ]; then update_platform; fi
+while [ "$INSTALLED" != "$VERSION" ]; do setup_platform
+done
+
+MENU_TEXT="\nChoose an option:\n"
+
+menu() {
+  # --menu <text> <height> <width> <listheight>
+  OPTION=$(whiptail --title "AviaTX Shell Script Menu" --menu "${MENU_TEXT}" 30 60 18 \
+  "01" "    Upgrade OS" \
+  "03" "    Full Install/Upgrade" \
+  "04" "    Platform Upgrade" \
+  "12" "    Change domain '${DOMAIN}'" \
+  "13" "    Change host alias '${HOSTALIAS}'" \
+  "00"  "    Exit"  3>&1 1>&2 2>&3)
+  EXITCODE=$?
+  [[ "$EXITCODE" = 1 ]] && break;
+  [[ "$EXITCODE" = 255 ]] && break;
+  # echo "exitcode: $EXITCODE"
+
+  case "$OPTION" in
+    "01") update_reboot_dialog; run_upgrade_playbook ;;
+    "03") run_platform_playbook full ;;
+    "04") run_platform_playbook pservice,ppart ;;
+    "12") request_domain ;;
+    "13") request_hostalias ;;
+    "00") exit 0 ;;
+    *) echo "Unknown action '${OPTION}'" ;;	
+  esac
+  # sleep 0.5
+}
+
+# initial configuration
+initialize
+# start menu loop
+while [ 1 ]; do
+  menu
+done
