0ed39b9d020615a96d448fd15ae1534b749addf5
andrewromm <andrew.romm@gmail.com>
2024-07-30 11:37:04 +0300

    up version;

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=4.0.2
+VERSION=4.0.3
 BOOTSTRAP_BRANCH=${BRANCH:-master}
 BOOTSTRAP_DIR=/srv/aviatx/bootstrap
 BOOTSTRAP_REPO=https://github.com/andrewromm/aviatx_setup.git
